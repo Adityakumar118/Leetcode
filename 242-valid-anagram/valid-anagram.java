43836e0db0d8038c5c28a350ff61dc15a1c857e0
Aditya Kumar <147988179+Adityakumar118@users.noreply.github.com>
2025-04-15 13:08:05 +0530

    Time: 4 ms (75.74%) | Memory: 44.8 MB (36.56%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,14 +3,14 @@ class Solution {
         // s= s.toLowerCase();
         // t = t.toLowerCase();
 
-        if(s.length()==t.length()){
+        //if(s.length()==t.length()){
             char[] s1charArray = s.toCharArray();
             char[] t2charArray = t.toCharArray();
 
             Arrays.sort(s1charArray);
             Arrays.sort(t2charArray);
             return Arrays.equals(s1charArray,t2charArray);
-        }
-        return false;
+        // }
+        // return false;
     }
 }
